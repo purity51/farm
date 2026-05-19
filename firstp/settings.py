4488008eafa0afeb562bc131b703a6960c48531e
purity7112 <puritymulinge451@gmail.com>
2026-05-19 16:03:31 +0300

    add changes

--- firstp/settings.py
+++ firstp/settings.py
@@ -48,6 +48,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
